1c2edcc9faf055d98c66e58dc8c69954fd5dec75
Deepak  Bairwa <150535253+Abhimanyu0201@users.noreply.github.com>
2026-05-16 15:36:35 +0530

    Update CORS configuration to allow localhost

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://ai-interviewer-frontend-to78.onrender.com",
+    origin: ["http://localhost:5173",
+        "https://ai-interviewer-frontend-to78.onrender.com"]
     credentials: true
 }))
 
