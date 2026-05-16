bfcfa65b424c75b9c8c863cacfdcf8478b4748eb
Deepak  Bairwa <150535253+Abhimanyu0201@users.noreply.github.com>
2026-05-16 11:21:54 +0530

    Update API base URL to production server

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-interviewer-zm6v.onrender.com",
     withCredentials: true
 })
 
